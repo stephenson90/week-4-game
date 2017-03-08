392e7305fc5b06c491ee9ada062cf3a09db7b17a
Jean Davilus <jsd6@njit.edu>
2017-03-08 04:47:30 -0500

    it is working

--- assets/javaScript/game.js
+++ assets/javaScript/game.js
@@ -6,34 +6,40 @@ $(document).ready(function(){
 	var score = 0;
 	var wins = 0;
 	var losses = 0;
-	var nbr = Math.floor(Math.random()*113) + 18;
-	var rdVal = Math.floor(Math.random()*12)+1;
-	var blVal = Math.floor(Math.random()*12)+1;
-	var yel = Math.floor(Math.random()*12)+1;
-	var grn = Math.floor(Math.random()*12)+1;
-	
-	
-	
-	
-	
+	var nbr;
+	var rdVal;
+	var blVal;
+	var yel;
+	var grn;
+
+
+	// var nbr = Math.floor(Math.random()*113) + 18;
+	// var rdVal = Math.floor(Math.random()*12)+1;
+	// var blVal = Math.floor(Math.random()*12)+1;
+	// var yel = Math.floor(Math.random()*12)+1;
+	// var grn = Math.floor(Math.random()*12)+1;	
 	
 
 
 	function setNbr(){
 
-	rdVal = Math.floor(Math.random()*12)+1 ;
-	blVal = Math.floor(Math.random()*12)+1;
-	yel = Math.floor(Math.random()*12)+1;
-	grn = Math.floor(Math.random()*12)+1;
-	nbr = Math.floor(Math.random()*113) + 18;
-	score=0;
-}
+		rdVal = Math.floor(Math.random()*12)+1 ;
+		blVal = Math.floor(Math.random()*12)+1;
+		yel = Math.floor(Math.random()*12)+1;
+		grn = Math.floor(Math.random()*12)+1;
+		nbr = Math.floor(Math.random()*113) + 18;
+		score=0;
 
-	
 	$("#ranNbr").html(nbr);
 	$("#wins").html("Wins: " + wins);
 	$("#losses").html("Losses: " + losses);
 	$("#scr").html(score);
+}
+
+setNbr();
+
+	
+	
 
 			$("#green").click(function(){
 			score = score + grn;
@@ -67,26 +73,23 @@ $(document).ready(function(){
 
 	function checkWin(){
 
-	if( score == nbr){
-		wins++;
-		$("#wins").html("Wins: " + wins);
-		setNbr();
-		
-
+		if( score === nbr){
+			wins++;
+			$("#wins").html("Wins: " + wins);
+			setNbr();
 
 
 	}
 
-	else if (score > nbr){
-		losses++;
-		$("#losses").html("Losses: " + losses);
-		setNbr();
+
+		else if (score > nbr){
+			losses++;
+			$("#losses").html("Losses: " + losses);
+			setNbr();
 
 	}
 }
 
-	console.log(score);
-		console.log(nbr);
 
 
 
